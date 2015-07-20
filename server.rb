3c279f085ce43fe6271214a754ea2831e0bb7312
Alec Greenberg <aleckyler@comcast.com>
2015-07-20 13:24:19 -0400

    first commit

---
+++ server.rb
@@ -0,0 +1,40 @@
+require "sinatra"
+
+get "/" do
+  erb :index
+end
+
+get "/about" do
+  @name = "Nameless"
+  erb :about
+end
+
+get "/about/*" do
+  @name = params['splat'].first
+  erb :about
+end
+
+get "/turtle/:id" do
+  turtles = [
+    "Tutley",
+    "Yertle",
+    "Stanley",
+    "Pookie"
+  ]
+  id = params["id"].to_i
+  @name = turtles[id]
+  erb :turtle
+end
+
+get "/projects/:id" do
+  projects = ["portfolio", "REPL Game", "Voting Simulation"]
+  @project = projects[params["id"].to_i]
+  "Here's my #{@project} page!"
+end
+
+get "/link/:id" do
+  links = {linkedin: "https://www.linkedin.com", github: "https://github.com", twitter: "https://twitter.com"}
+  id = params["id"].to_s
+  @link = links[id.to_sym]
+  erb :link
+end
